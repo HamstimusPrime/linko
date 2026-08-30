37a08b096229b594156a1eaf9735b64999729612
Mohammed Dadi <artofmohammed@gmail.com>
2026-08-30 03:10:20 +0100

    feat(metrics): Add metric monitor for counting HTTP requests

--- auth.go
+++ auth.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	pkgerr "github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -83,5 +86,42 @@ func requestID(next http.Handler) http.Handler {
 		}
 		w.Header().Set("X-Request-ID", requestID)
 		next.ServeHTTP(w, r)
+	})
+}
+
+var httpRequestTotal = promauto.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Total number of HTTP requests.",
+	},
+	[]string{"method", "path", "status"},
+)
+
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (r *statusRecorder) WriteHeader(code int) {
+	r.status = code
+	r.ResponseWriter.WriteHeader(code)
+}
+
+func metricsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		rec := &statusRecorder{
+			ResponseWriter: w,
+			status:         http.StatusOK,
+		}
+
+		next.ServeHTTP(rec, r)
+
+		path := r.URL.Path
+		method := r.Method
+		status := strconv.Itoa(rec.status)
+
+		httpRequestTotal.WithLabelValues(method, path, status).Inc()
+
 	})
 }
--- server.go
+++ server.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"boot.dev/linko/internal/store"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type server struct {
@@ -180,7 +181,7 @@ func newServer(store store.Store, port int, logger *slog.Logger, cancel context.
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
-		Handler: requestID(requestLogger(logger)(mux)),
+		Handler: metricsMiddleware(requestID(requestLogger(logger)(mux))),
 	}
 
 	s := &server{
@@ -190,6 +191,7 @@ func newServer(store store.Store, port int, logger *slog.Logger, cancel context.
 		logger:     logger,
 	}
 
+	mux.Handle("GET /metrics", promhttp.Handler())
 	mux.HandleFunc("GET /", s.handlerIndex)
 	mux.Handle("POST /api/login", s.authMiddleware(http.HandlerFunc(s.handlerLogin)))
 	mux.Handle("POST /api/shorten", s.authMiddleware(http.HandlerFunc(s.handlerShortenLink)))
@@ -200,4 +202,3 @@ func newServer(store store.Store, port int, logger *slog.Logger, cancel context.
 
 	return s
 }
-
